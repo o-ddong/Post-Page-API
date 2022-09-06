541052ecf4d0f34e716a7a4c061bbd7872823e1c
o-ddong <odh0112@naver.com>
2022-09-06 23:00:27 +0900

    feat: user model 추가

--- application/user/models.py
+++ application/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from application.base.models import BaseModel
+
+class User(BaseModel):
+    """
+    유저 모델
+    """
+    id = models.IntegerField(primary_key=True)
+    password = models.CharField(max_length=20, verbose_name='비밀번호')
+    
