86619ea002f08362bd80de498ed2a53a9163ae83
o-ddong <odh0112@naver.com>
2022-09-06 22:59:52 +0900

    feat: 게시글 model 추가

--- application/board/models.py
+++ application/board/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from application.base.models import BaseModel
+
+class Post(BaseModel):
+    """
+    게시글 모델
+    """
+    title = models.CharField(max_length=20, verbose_name='제목')
+    content = models.CharField(max_length=200, verbose_name='본문')
+
