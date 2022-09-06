af7a615ed91d1d4aabc9988a8d1f6a0e04fdf603
o-ddong <odh0112@naver.com>
2022-09-07 01:10:24 +0900

    style: code rename

--- applications/board/views.py
+++ applications/board/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import mixins, generics
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
 
-from application.board.models import Post
-from application.board.serializer import PostSerializer
+from applications.board.models import Post
+from applications.board.serializer import PostModelSerializer
 
 
 class PostAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
@@ -12,11 +14,17 @@ class PostAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.Gener
     게시글 생성 및 조회
     """
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = PostModelSerializer
 
     def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+        return self.list(request)
 
     def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+        try:
+            return self.create(request)
+        except Exception as e:
+            print(e)
+            return Response("게시글이 생성되지 않았습니다.")
+
+
 
