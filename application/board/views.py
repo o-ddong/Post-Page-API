cf005dd57ae2c3f033a00753077d509bbe30a8ac
o-ddong <odh0112@naver.com>
2022-09-06 23:43:41 +0900

    feat: 게시글 생성 및 조회 추가

--- application/board/views.py
+++ application/board/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import mixins, generics
+
+from application.board.models import Post
+from application.board.serializer import PostSerializer
+
+
+class PostAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    """
+    게시글 생성 및 조회
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
