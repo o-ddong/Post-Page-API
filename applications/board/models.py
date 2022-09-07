36716ebb4c4c42ec0ab09974eb0433f9b3f8400d
o-ddong <odh0112@naver.com>
2022-09-07 23:46:19 +0900

    fix: DB 구조 수정

--- applications/board/models.py
+++ applications/board/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
 # Create your models here.
-from applications.base.models import BaseModel
 
-class Post(BaseModel):
+class Post(models.Model):
     """
     게시글 모델
     """
-    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=20, verbose_name='제목')
     content = models.CharField(max_length=200, verbose_name='본문')
+    password = models.CharField(max_length=20, verbose_name="비밀번호 여부")
+    weather = models.CharField(max_length=20, null=True, verbose_name="현재 날씨")
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name='생성일')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정일')
 
     def __str__(self):
         return str(self.id)
