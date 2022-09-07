0f645ec21488df3c851e486dd7892072496d24b1
o-ddong <odh0112@naver.com>
2022-09-07 23:47:45 +0900

    feat: 세부사항 수정 및 날씨 API 기능 추가

--- applications/board/views.py
+++ applications/board/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,8 +8,9 @@ from rest_framework.response import Response
 
 from applications.board.models import Post
 from applications.board.serializer import PostSerializer
-from applications.user.models import User
+from django.core.paginator import Paginator
 from argon2 import PasswordHasher
+import re
 
 
 class PostAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
@@ -17,49 +19,67 @@ class PostAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.Gener
     - 제목 최대 20자, 본문 최대 200자 제한
     - 제목 && 본문 이모지 포함 가능
     """
-    queryset = Post.objects.all()
+    queryset = Post.objects.all().order_by("-id")
+    page = Paginator(queryset, 20)
     serializer_class = PostSerializer
 
     def get(self, request, *args, **kwargs):
-        print(int(str(self.queryset.reverse().first())) + 1)
         return self.list(request)
 
     def post(self, request, *args, **kwargs):
         try:
-            title = request.data['title']
-            content = request.data['content']
-            password = request.POST.get('password')
+            data = request.data.copy()
+            title = data['title']
+            content = data['content']
+            password = data['password']
 
             if len(title) > 20:
                 return Response("제목이 20자를 초과합니다.")
             elif len(content) > 200:
                 return Response("본문이 200자를 초과합니다.")
+            if not password_convertion(password):
+                return Response("비밀번호 양식에 맞지 않습니다.")
+            if len(password) < 6:
+                return Response("비밀번호가 너무 짧습니다.")
 
-            self.create(request)
-            if password:
-                user = User()
-                user.post = self.queryset.reverse().first()
-                # user.post = int(str(self.queryset.reverse().first())) + 1
-                user.password = PasswordHasher().hash(password)
-                user.save()
-            return Response("성공적으로 생성되었습니다.")
+            # weather = send_weather_api()
+
+            return self.create(request)
         except Exception as e:
             print(e)
             return Response("게시글이 정상적으로 생성되지 않았습니다.")
 
-class UserDetailMixins(mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class PostDetailDeleteMixins(mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
     """
     게시글 정보 수정 및 탈퇴
     """
-
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    def put(self, request, *args, **kwargs):
+    def put(self, request, pk, *args, **kwargs):
+        password = request.data['password']
+        queryset = Post.objects.filter(id=pk, password=password)
+        if not queryset:
+            return Response("해당하는 게시물이 존재하지 않습니다.")
         return self.update(request, *args, **kwargs)
 
-    def delete(self, request, *args, **kwargs):
-        # try:
-        #     queryset = User.objects.select_related('password')
+    def delete(self, request, pk, *args, **kwargs):
+        password = request.data['password']
+        queryset = Post.objects.filter(id=pk, password=password)
+        if not queryset:
+            return Response("해당하는 게시물이 존재하지 않습니다.")
         return self.destroy(request, *args, **kwargs)
 
+
+def password_convertion(pw):
+    """ 패스워드 양식 체크 """
+    result = re.compile('[0-9]')
+    if len(result.findall(pw)) < 1:
+        return False
+    return True
+
+def send_weather_api():
+    URL = 'http://api.weatherapi.com/v1/current.json?key=189801d7b61741d797e135108220709&q=Korea&aqi=no'
+    response = requests.get(URL)
+    data = response.json()
+    return data['current']['condition']['text']
